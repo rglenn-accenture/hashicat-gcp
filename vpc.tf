63b415d8d6a36c0147a4e0c6ef3aed684c1d283f
Robert Glenn <robert.glenn@accenture.com>
2023-03-19 19:37:34 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
     source  = "terraform-google-modules/network/google"
-    version = "~> 6.0"
+    version = "~> 3.0"
 
     project_id   = var.project
     network_name = "gaurav-network"
@@ -13,17 +13,6 @@ module "vpc" {
             subnet_region = var.region
         }
     ]
-
-    secondary_ranges = {
-        subnet-01 = [
-            {
-                range_name    = "subnet-01-secondary-01"
-                ip_cidr_range = "192.168.64.0/24"
-            },
-        ]
-
-        subnet-02 = []
-    }
 
     routes = [
         {
